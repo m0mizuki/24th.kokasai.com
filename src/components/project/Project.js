00c86db7c4c2e8fdd9c91bc9dcbc0fa9cece993b
m0mizuki <mutou.m.2020@gmail.com>
2023-08-17 17:52:48 +0900

    企画ページにスクロールバーを追加

--- src/components/project/Project.js
+++ src/components/project/Project.js
@@ -1,11 +1,11 @@
 import { Pages } from "../Pages";
 import backGround from "../../img/backGround/space.jpg"
 import "../../css/pageStyle.css";
+import "./projectStyle.css";
 
 function Project() {
-  function getApi(){
-    //var voteData=fetch("https://forms.googleapis.com/v1/forms/XYP-cpVeEkWK4KezivJfyHxakDBs24lNpXOoUtJPL1RUMUpCN0lWTUZaWUg1OEgxRlc4NkNJTEtDRi4u/responses").json;
-    //https://forms.googleapis.com/v1/forms/XYP-cpVeEkWK4KezivJfyHxakDBs24lNpXOoUtJPL1RUMUpCN0lWTUZaWUg1OEgxRlc4NkNJTEtDRi4u/responses
+
+  /*function getApi(){
     const request = new XMLHttpRequest();
     request.open("GET", "https://dog.ceo/api/breeds/image/random",true);
     
@@ -19,17 +19,30 @@ function Project() {
       img.src=apiData.message;
 
     });
-  }
+  }*/
 
   return (
     <>
-      <h1>{Pages.project.displayName}</h1>
-      <img src={backGround} className="backGroundImage responsiveWidthS" />
+      <img src={backGround} className="backGroundImage responsiveWidth" />
 
-      <img src="" id="dogImage" />
+      <div className="moitonArea responsiveWidth">
+        <div className="projectSelectBar">
+          <button className="projectSelectButton">1年生</button>
+          <button className="projectSelectButton">2年生</button>
+          <button className="projectSelectButton">3年生</button>
+          <button className="projectSelectButton">4年生</button>
+          <button className="projectSelectButton">5年生</button>
+          <button className="projectSelectButton">運動部①</button>
+          <button className="projectSelectButton">運動部②</button>
+          <button className="projectSelectButton">文化部①</button>
+          <button className="projectSelectButton">文化部②</button>
+          <button className="projectSelectButton">愛好会①</button>
+          <button className="projectSelectButton">愛好会②</button>
+          <button className="projectSelectButton">有志</button>
+        </div>
+      </div>
 
       <div className="contents">
-        <button onClick={getApi}>formsのapiを取得</button>
         <p>あいうえお</p><br />
         <p>かきくけこ</p><br />
         <p>さしすせそ</p><br />
