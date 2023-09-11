352fd26d73cc6cef2f8884abb1b454a108c7472b
m0mizuki <mutou.m.2020@gmail.com>
2023-09-11 18:22:43 +0900

    change: 見出しのデザインを変更

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -16,78 +16,84 @@ function Home() {
       </div>
 
       <div className="contents contents_whitesmoke">
-        <h2>詳細情報</h2>
-        <h3>開催日時</h3>
-        <p>11月4日(土) 9:00～17:00<br />
-          11月5日(日) 9:00～17:00</p>
+        <div className="contents_innerBlock">
+          <br />
+          <p className="heading2">詳細情報</p>
+          <p className="heading3">開催日時</p>
+          <p>11月4日(土) 9:00～17:00<br />
+            11月5日(日) 9:00～17:00</p>
 
-        <h3>開催場所</h3>
-        <p>群馬工業高等専門学校</p>
-        <p><a href="https://www.gunma-ct.ac.jp/" target="_blank" rel="noreferrer noopener">公式HPはこちら</a></p>
+          <p className="heading3">開催場所</p>
+          <p>群馬工業高等専門学校</p>
+          <p><a href="https://www.gunma-ct.ac.jp/" target="_blank" rel="noreferrer noopener">公式HPはこちら</a></p>
 
-        <h3>注意事項</h3>
-        <p>酒類の持ち込みはご遠慮ください</p>
+          <p className="heading3">注意事項</p>
+          <p>酒類の持ち込みはご遠慮ください</p>
 
-        <h2>役員あいさつ</h2>
-        <details className="portraitDetails">
-          <summary className="portraitSummary">
-            <div className="portraitFrame">
-              <img className="portraitImage" src={portrait_sakana} />
-            </div>
-            <div className="portraitTitle">
-              <p>工華祭実行委員長 本島琉矢</p>
-              <h4>タイトル</h4>
-            </div>
-            <img className="portraitArrow" src={portraitArrow} />
-          </summary>
-          <p>こんにちはこんにちはこんにちはこんにちはこんにちはこんにちは
-            こんにちはこんにちはこんにちはこんにちはこんにちはこんにちは</p>
-        </details>
+          <p className="heading2">役員あいさつ</p>
+          <details className="portraitDetails">
+            <summary className="portraitSummary">
+              <div className="portraitFrame">
+                <img className="portraitImage" src={portrait_sakana} />
+              </div>
+              <div className="portraitTitle">
+                <p>工華祭実行委員長 本島琉矢</p>
+                <h4>タイトル</h4>
+              </div>
+              <img className="portraitArrow" src={portraitArrow} />
+            </summary>
+            <p>こんにちはこんにちはこんにちはこんにちはこんにちはこんにちは
+              こんにちはこんにちはこんにちはこんにちはこんにちはこんにちは</p>
+          </details>
 
-        <details className="portraitDetails">
-          <summary className="portraitSummary">
-            <div className="portraitFrame">
-              <img className="portraitImage" src={portrait_sakana} />
-            </div>
-            <div className="portraitTitle">
-              <p>工華祭実行委員長 本島琉矢</p>
-              <h4>タイトル</h4>
-            </div>
-            <img className="portraitArrow" src={portraitArrow} />
-          </summary>
-          <p>こんにちはこんにちはこんにちはこんにちはこんにちはこんにちは
-            こんにちはこんにちはこんにちはこんにちはこんにちはこんにちは</p>
-        </details>
+          <details className="portraitDetails">
+            <summary className="portraitSummary">
+              <div className="portraitFrame">
+                <img className="portraitImage" src={portrait_sakana} />
+              </div>
+              <div className="portraitTitle">
+                <p>工華祭実行委員長 本島琉矢</p>
+                <h4>タイトル</h4>
+              </div>
+              <img className="portraitArrow" src={portraitArrow} />
+            </summary>
+            <p>こんにちはこんにちはこんにちはこんにちはこんにちはこんにちは
+              こんにちはこんにちはこんにちはこんにちはこんにちはこんにちは</p>
+          </details>
 
-        <details className="portraitDetails">
-          <summary className="portraitSummary">
-            <div className="portraitFrame">
-              <img className="portraitImage" src={portrait_sakana} />
-            </div>
-            <div className="portraitTitle">
-              <p>工華祭実行委員長 本島琉矢</p>
-              <h4>タイトル</h4>
-            </div>
-            <img className="portraitArrow" src={portraitArrow} />
-          </summary>
-          <p>こんにちはこんにちはこんにちはこんにちはこんにちはこんにちは
-            こんにちはこんにちはこんにちはこんにちはこんにちはこんにちは</p>
-        </details>
+          <details className="portraitDetails">
+            <summary className="portraitSummary">
+              <div className="portraitFrame">
+                <img className="portraitImage" src={portrait_sakana} />
+              </div>
+              <div className="portraitTitle">
+                <p>工華祭実行委員長 本島琉矢</p>
+                <h4>タイトル</h4>
+              </div>
+              <img className="portraitArrow" src={portraitArrow} />
+            </summary>
+            <p>こんにちはこんにちはこんにちはこんにちはこんにちはこんにちは
+              こんにちはこんにちはこんにちはこんにちはこんにちはこんにちは</p>
+          </details>
 
-        <details className="portraitDetails">
-          <summary className="portraitSummary">
-            <div className="portraitFrame">
-              <img className="portraitImage" src={portrait_sakana} />
-            </div>
-            <div className="portraitTitle">
-              <p>工華祭実行委員長 本島琉矢</p>
-              <h4>タイトル</h4>
-            </div>
-            <img className="portraitArrow" src={portraitArrow} />
-          </summary>
-          <p>こんにちはこんにちはこんにちはこんにちはこんにちはこんにちは
-            こんにちはこんにちはこんにちはこんにちはこんにちはこんにちは</p>
-        </details>
+          <details className="portraitDetails">
+            <summary className="portraitSummary">
+              <div className="portraitFrame">
+                <img className="portraitImage" src={portrait_sakana} />
+              </div>
+              <div className="portraitTitle">
+                <p>工華祭実行委員長 本島琉矢</p>
+                <h4>タイトル</h4>
+              </div>
+              <img className="portraitArrow" src={portraitArrow} />
+            </summary>
+            <p>こんにちはこんにちはこんにちはこんにちはこんにちはこんにちは
+              こんにちはこんにちはこんにちはこんにちはこんにちはこんにちは</p>
+          </details>
+
+          <br />
+        </div>
+
 
       </div>
     </>
