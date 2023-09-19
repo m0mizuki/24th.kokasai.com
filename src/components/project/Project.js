3aaaba15fe44a974bc702e704ac87f0a928830a7
m0mizuki <mutou.m.2020@gmail.com>
2023-09-19 12:02:22 +0900

    change: 企画ページを星が回転するアニメ－ジョンに変更

--- src/components/project/Project.js
+++ src/components/project/Project.js
@@ -32,24 +32,25 @@ function Project() {
     }
 
     CreatePlanets(initGalNum); //初期設定:1年生の星を表示
-    //ラベルを表示
+
+    const planetArea = document.getElementById("planetArea");
+    planetArea.style.animationTimingFunction = "ease-out";
+    planetArea.style.animationName = "fadeIn";
+
     setTimeout(() => {
+      //ラベルを表示
       let planetText = document.getElementsByClassName("planetText");
       for (let i = 0; i < planetText.length; i++) {
         planetText[i].classList.remove("invisible");
       }
-
-      //セレクトバーを表示
-      let projectSelectBar = document.getElementById("projectSelectBar");
-      projectSelectBar.classList.remove("invisible");
-    }, 1400);
-
+    }, 600);
 
   }, []);
 
   //最終的にはcreateElementではなくひとまとまりの要素を追加できるようにしたい
 
   function changeGalaxy(galaxyNum) {
+    const planetArea = document.getElementById("planetArea");
 
     //ラベルを非表示
     let planetText = document.getElementsByClassName("planetText"); //競合注意
@@ -57,19 +58,16 @@ function Project() {
       planetText[i].classList.add("invisible");
     }
 
-    //アニメーションを追加
-
-    let planetBox = document.getElementsByClassName("planetBox"); //競合注意
-    for (let i = 0; i < planetBox.length; i++) {
-      //planetBox[i].classList.add("boxMotion_out");
-      planetBox[i].style.animationDelay = 0 + "s";
-      planetBox[i].style.animationName = "boxAnimation_out" + ((i % (planetBox.length/2)) + 1);
-      //console.log((i % (planetBox.length/2)) + 1);
-    }
+    planetArea.style.animationTimingFunction = "ease-in";
+    planetArea.style.animationName = "fadeOut";
 
     setTimeout(() => {
       CreatePlanets(galaxyNum);
-    }, 700);
+
+      planetArea.style.animationTimingFunction = "ease-out";
+      planetArea.style.animationName = "fadeIn";
+
+    }, 800);
 
     setTimeout(() => {
       //ラベルを表示
@@ -77,9 +75,10 @@ function Project() {
       for (let i = 0; i < planetText.length; i++) {
         planetText[i].classList.remove("invisible");
       }
-    }, 2300);
+    }, 800 + 600);
 
   }
+
 
   function CreatePlanets(galaxyNum) {
     const planetArea = document.getElementById("planetArea");
@@ -93,24 +92,6 @@ function Project() {
     //新たな星を作成
 
     var leftRatio = new Array(galaxyNum);
-    for (let i = 1; i < projectData[galaxyNum].length; i++) {
-      //planetImage,planetTextはplanetBoxの子要素
-      //planetBoxがplanetAreaの子要素になる
-      let planetBox = document.createElement("div");
-      planetBox.classList.add("planetBox");
-      planetBox.classList.add("boxMotion" + i);
-      leftRatio[i - 1] = 1 + 17 * (i - 1) + Math.random() * 10 - 5;
-      planetBox.style.left = leftRatio[i - 1] + "%";
-
-      let planetRope = document.createElement("img");
-      planetRope.src = rope;
-      planetRope.classList.add("planetRope");
-      planetRope.style.animationDelay = parseInt(4000 * i / (projectData[galaxyNum].length - 1)) + "ms";
-
-      planetBox.appendChild(planetRope);
-
-      planetArea.appendChild(planetBox);
-    }
 
     for (let i = 1; i < projectData[galaxyNum].length; i++) {
       //planetImage,planetTextはplanetBoxの子要素
@@ -126,7 +107,7 @@ function Project() {
 
       let planetImage = document.createElement("img");
       //planetImage.src = planet_1;
-      const planetNum = (i * 13 + galaxyNum*9) % 7;
+      const planetNum = (i * 13 + galaxyNum * 9) % 7;
       planetImage.src = "./img/planet/planet_" + planetNum + ".png";
       planetImage.classList.add("planetImage");
       planetImage.style.animationDelay = parseInt(4000 * i / (projectData[galaxyNum].length - 1)) + "ms";
@@ -145,7 +126,74 @@ function Project() {
       planetArea.appendChild(planetBox);
     }
 
+    //位置を設定
+    addRad = 0;
+    SetPlanets(addRad);
   }
+
+  //星の位置を設定
+  function SetPlanets(addRad) {
+    let planetBox = document.getElementsByClassName("planetBox");
+
+    if (planetBox.length == 0) {
+      //console.log("ない");
+    }
+    else {
+      for (let i = 0; i < planetBox.length; i++) {
+        const rad = i * 2 * Math.PI / planetBox.length + addRad;
+
+        const boxWidth = 30 + 10 * Math.cos(rad);
+
+        planetBox[i].style.left = 50 - boxWidth / 2 + 35 * Math.sin(rad) + "%";
+        planetBox[i].style.top = 45 - boxWidth / 2 + 15 * Math.cos(rad) + "%";
+
+        planetBox[i].style.zIndex = 1000 + parseInt(100 * Math.cos(rad));
+
+        planetBox[i].style.width = boxWidth + "%";
+        planetBox[i].style.height = boxWidth + "%";
+      }
+    }
+
+  }
+
+  var prePos = { x: 0, y: 0 };
+  var addRad = 0; //角度(ラジアン)
+
+  //置かれた指の位置を取得
+  function SetPrePos(e) {
+    //e.preventDefault();
+    prePos.x = e.touches[0].clientX;
+    prePos.y = e.touches[0].clientY;
+
+    SetPlanets(addRad);
+  }
+
+  //スクロールした際の星の移動
+  function RotatePlanets(e) {
+    e.preventDefault();
+    const pos = {
+      x: e.touches[0].clientX,
+      y: e.touches[0].clientY
+    };
+
+    addRad += (pos.x - prePos.x) / 100;
+    prePos.x = pos.x;
+    prePos.y = pos.y;
+    SetPlanets(addRad);
+  }
+
+
+  //パッシブでない関数を呼び出す
+  const circleRef = useRef(null);
+  useEffect(() => {
+    circleRef.current.addEventListener("touchstart", SetPrePos, { passive: false });
+    circleRef.current.addEventListener("touchmove", RotatePlanets, { passive: false });
+    return (() => {
+      circleRef.current.removeEventListener("touchstart", SetPrePos);
+      circleRef.current.removeEventListener("touchmove", RotatePlanets);
+    });
+  });
+
 
 
   return (
@@ -153,8 +201,8 @@ function Project() {
       <img src={backGround} className="backGroundImage responsiveWidth" />
 
       <div className="moitonArea responsiveWidth">
-        <div id="projectSelectBar" className="projectSelectBar invisible"></div>
-        <div id="planetArea" className="planetArea"></div>
+        <div id="projectSelectBar" className="projectSelectBar"></div>
+        <div id="planetArea" className="planetArea" ref={circleRef}></div>
       </div>
 
       <div className="contents">
