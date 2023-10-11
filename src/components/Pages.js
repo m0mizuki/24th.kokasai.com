3b3c4f65f0ea7e252f22a6a84cf58aba0bf2484f
m0mizuki <mutou.m.2020@gmail.com>
2023-10-11 10:46:47 +0900

    change: メニューバーの画像を変更

--- src/components/Pages.js
+++ src/components/Pages.js
@@ -2,27 +2,27 @@ export const Pages={
     access:{
         displayName:"アクセス",
         path:"/24th.kokasai.com/access",
-        icon:`${process.env.PUBLIC_URL}/img/menuBarIcon/astronaut.png`,
+        icon:`${process.env.PUBLIC_URL}/img/menuBarIcon/accessIcon.png`,
     },
     event:{
         displayName:"ステージ企画",
         path:"/24th.kokasai.com/event",
-        icon:`${process.env.PUBLIC_URL}/img/menuBarIcon/astronaut.png`,
+        icon:`${process.env.PUBLIC_URL}/img/menuBarIcon/eventIcon.png`,
     },
     home:{
         displayName:"ホーム",
         path:"/24th.kokasai.com",
-        icon:`${process.env.PUBLIC_URL}/img/menuBarIcon/astronaut.png`,
+        icon:`${process.env.PUBLIC_URL}/img/menuBarIcon/homeIcon.png`,
     },
     map:{
         displayName:"マップ",
         path:"/24th.kokasai.com/map",
-        icon:`${process.env.PUBLIC_URL}/img/menuBarIcon/astronaut.png`,
+        icon:`${process.env.PUBLIC_URL}/img/menuBarIcon/mapIcon.png`,
     },
     project:{
         displayName:"企画",
         path:"/24th.kokasai.com/project",
-        icon:`${process.env.PUBLIC_URL}/img/menuBarIcon/astronaut.png`,
+        icon:`${process.env.PUBLIC_URL}/img/menuBarIcon/projectIcon.png`,
     },
     credit:{
         displayName:"クレジット",
