a66c6761e802b6191564f19dc8645d0c1ae1eaa2
m0mizuki <mutou.m.2020@gmail.com>
2023-10-11 12:37:12 +0900

    change: React.StrictModeを追加

--- src/index.js
+++ src/index.js
@@ -6,15 +6,11 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <React.StrictMode>
     <App />
+  </React.StrictMode>
 );
 
-//正式に公開する際にはこうする
-/*
-<React.StrictMode>
-  <App />
-</React.StrictMode>
-*/
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
